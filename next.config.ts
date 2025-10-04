b600d3f538b401f1b08b19167f35098f5a373980
Gabriel R. Antunes <gabrielrodantunes.dev@gmail.com>
2025-10-04 17:26:38 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1] || "venda-pasteis";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true,
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
 };
 
 export default nextConfig;
